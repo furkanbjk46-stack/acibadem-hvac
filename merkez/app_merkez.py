7fcbb0fb670c54c04637b3ec47c2b7168b68ee69
furkanbjk46-stack <furkanbjk46@gmail.com>
2026-06-21 16:26:54 +0300

    Block-container yan boslugu 0'dan 100px'e cikarildi

--- merkez/app_merkez.py
+++ merkez/app_merkez.py
@@ -75,7 +75,7 @@ header[data-testid="stHeader"] button    { display: none !important; }
     height: 100vh !important;
     max-height: 100vh !important;
     overflow: hidden !important;
-    padding: 0 0 0.2rem 0 !important;
+    padding: 0 100px 0.2rem 100px !important;
     margin-top: 0 !important;
     max-width: 100% !important;
     box-sizing: border-box !important;
